f7028afcfbdcd2739c6954d275dca7681da3bdab
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-20 10:59:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let button = document.getElementsByTagName("button")[0];
+let p = document.getElementsByTagName("p")[0];
+button.addEventListener("click",change);
+function change(event){
+	let newHeading = document.createElement("h1");
+	newHeading.innerText = p.innerText;
+	p.parentNode.replaceChild(newHeading, p);
+}
